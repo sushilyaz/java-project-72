555422c880075bb748a998e2d8dbb7040f1fa96d
suhoi <sushilyaz2000@gmail.com>
2023-12-19 01:14:41 +0300

    add Docker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,19 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
     application
     checkstyle
     jacoco
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
 application {
-    mainClass = "hexlet.code.Main"
+    mainClass = "hexlet.code.App"
 }
 
 repositories {
@@ -19,11 +23,30 @@ tasks.withType<JavaCompile> {
     options.release.set(20)
 }
 dependencies {
+    implementation("com.h2database:h2:2.2.222")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("org.apache.commons:commons-text:1.10.0")
+    implementation("gg.jte:jte:3.1.0")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("io.javalin:javalin:5.6.2")
+    implementation("io.javalin:javalin-bundle:5.6.2")
+    implementation("io.javalin:javalin-rendering:5.6.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
     finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
 tasks.jacocoTestReport {
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,22 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+import java.util.List;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        Javalin app = Javalin.create();
+        app.get("/", ctx -> {
+            ctx.result("Hello World");
+        });
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
